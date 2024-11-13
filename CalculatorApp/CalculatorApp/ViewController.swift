ba438e0abff33b7bc779a6980c0642697ea9f577
Youngjin <bryjna07@gmail.com>
2024-11-13 09:37:01 +0900

    [feat] lv 1 주석 추가

--- CalculatorApp/CalculatorApp/ViewController.swift
+++ CalculatorApp/CalculatorApp/ViewController.swift
@@ -10,25 +10,35 @@ import SnapKit
 
 class ViewController: UIViewController {
     
+    // 라벨 생성
     let label = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // viewDidLoad 될 때 configureUI 실행하여 라벨이 보이도록 함
         configureUI()
         
         
     }
     
     private func configureUI() {
+        
+        // 배경 검은색
         view.backgroundColor = .black
+        
+        // 라벨 특성 관련
         label.text = "12345"
         label.textColor = .white
         label.font = .boldSystemFont(ofSize: 60)
         label.textAlignment = .right
         
+        // addSubview 추가로 나타나게 함 ( 클로저 사용으로 추후 추가될 것들에 재사용성 대비 )
         [label]
             .forEach { view.addSubview($0) }
         
+        
+        // SnapKit 을 활용한 AutoLayOut & Constraints 설정
         label.snp.makeConstraints {
             $0.top.equalToSuperview().inset(200)
             $0.trailing.equalToSuperview().inset(30)
