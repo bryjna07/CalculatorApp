1d0031cc076adfda41177716fe302a733cb3871b
youngjin <bryjna07@nate.com>
2024-11-18 21:52:28 +0900

    [modify] Lv2 코드 수정

--- CalculatorApp/CalculatorApp/ViewController.swift
+++ CalculatorApp/CalculatorApp/ViewController.swift
@@ -39,33 +39,13 @@ class ViewController: UIViewController {
         label.font = .boldSystemFont(ofSize: 60)
         label.textAlignment = .right
         
-        // 버튼 특성 설정
-        button1.backgroundColor = UIColor(red: 50/255, green: 58/255, blue: 58/255, alpha: 1.0)
-        button1.setTitle("7", for: .normal)
-        button1.layer.cornerRadius = 4
+        let firstButtons = createButtons(titles: ["7", "8", "9", "+"])
         
-        button2.backgroundColor = UIColor(red: 50/255, green: 58/255, blue: 58/255, alpha: 1.0)
-        button2.setTitle("8", for: .normal)
-        button2.layer.cornerRadius = 4
-        
-        button3.backgroundColor = UIColor(red: 50/255, green: 58/255, blue: 58/255, alpha: 1.0)
-        button3.setTitle("9", for: .normal)
-        button3.layer.cornerRadius = 4
-        
-        button4.backgroundColor = UIColor(red: 50/255, green: 58/255, blue: 58/255, alpha: 1.0)
-        button4.setTitle("+", for: .normal)
-        button4.layer.cornerRadius = 4
-        
-        let buttons = [button1, button2, button3, button4]
-        
-        let hstackview = makeHorizontalStackView(buttons)
+        let hstackview = makeHorizontalStackView(firstButtons)
         
         // addSubview 추가로 나타나게 함 ( 클로저 사용으로 추후 추가될 것들에 재사용성 대비 )
         [label, hstackview]
             .forEach { view.addSubview($0) }
-        // 버튼 스택뷰에 추가
-        [button1, button2, button3, button4]
-            .forEach { hstackview.addArrangedSubview($0) }
         
         // SnapKit 을 활용한 AutoLayOut & Constraints 설정
         label.snp.makeConstraints {
@@ -86,6 +66,18 @@ class ViewController: UIViewController {
         
     }
     
+    // 버튼 생성 메서드
+    private func createButtons(titles: [String]) -> [UIButton] {
+        return titles.map { title in
+            let button = UIButton()
+            button.setTitle(title, for: .normal)
+            button.titleLabel?.font = .boldSystemFont(ofSize: 30)
+            button.backgroundColor = UIColor(red: 50/255, green: 58/255, blue: 58/255, alpha: 1.0)
+            button.layer.cornerRadius = 4
+            
+            return button
+        }
+    }
    //  horizontalStackView 생성하는 메서드
         private func makeHorizontalStackView(_ views: [UIView]) -> UIStackView {
             let stackView = UIStackView(arrangedSubviews: views)
